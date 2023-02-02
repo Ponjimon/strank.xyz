47a2f6ecd6caf1fb9e739a656c9ba00d932ab93f
Simon <770632+Ponjimon@users.noreply.github.com>
2023-02-02 22:40:51 +0000

    feat: add seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,45 @@
+import Head from 'next/head';
 import React from 'react';
+import { NextSeo } from 'next-seo';
 import strank from './strank.png';
 
+const title = 'Stockholm Snakes - Get Strank or Go Home';
+const description = 'Strank Snakes from Stockholm';
+const url = 'https://strank.xyz/';
+
 const Index = () => (
-  <div
-    className="w-screen h-screen bg-black"
-    style={{
-      background: `url('${strank.src}') no-repeat center`,
-      backgroundSize: 'cover',
-    }}
-  />
+  <>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <NextSeo
+      title={title}
+      description={description}
+      canonical={url}
+      openGraph={{
+        url,
+        title,
+        description,
+        images: [
+          {
+            url: `${url}/strank.png`,
+            width: 1024,
+            height: 1024,
+            alt: 'Stockholm Snakes Logo - Strank',
+            type: 'image/png',
+          },
+        ],
+        siteName: 'Stockholm Snakes - Strank',
+      }}
+    />
+    <div
+      className="w-screen h-screen bg-black"
+      style={{
+        background: `url('${strank.src}') no-repeat center`,
+        backgroundSize: 'cover',
+      }}
+    />
+  </>
 );
 
 export default Index;
