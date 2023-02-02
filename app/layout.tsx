7c333a3b60f230ee44302dde2846bf1d5e1933e9
Simon <770632+Ponjimon@users.noreply.github.com>
2023-02-02 22:18:25 +0000

    chore: init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import '../styles/global.css';
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-black">{children}</body>
     </html>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import Image from 'next/image';
+import strank from './strank.png';
 
-const Index = () => <>Hello world!</>;
+const Index = () => (
+  <div
+    className="w-screen h-screen bg-black"
+    style={{
+      background: `url('${strank.src}') no-repeat center`,
+      backgroundSize: 'cover',
+    }}
+  />
+);
 
 export default Index;
